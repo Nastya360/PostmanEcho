c5d9cb030802cd1a8168da71a3c9a1ffa738798c
Neto test <anastasiya.parfinenko@gmail.com>
2023-04-30 23:32:52 +0600

    return

--- src/test/java/PostmanTest.java
+++ src/test/java/PostmanTest.java
@@ -10,7 +10,7 @@ public class PostmanTest {
 // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("hello") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("dog") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
